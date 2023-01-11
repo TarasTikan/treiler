2d7277c32268c4fbd38016f2489e4a79e1032507
TarasTikan <tikantaras1@gmail.com>
2023-01-11 14:21:08 +0200

    add adad

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@ import { treiler, trend } from './fetch.js'
 // const BASE_URL = 'https://api.themoviedb.org/3';
 // const TREND_URL = `${BASE_URL}/trending/movie/week?api_key=`;
 // const TREILERS = `${BASE_URL}/movie/899112/videos?api_key=`;
-// const list = document.querySelector('.list-films')
+const list = document.querySelector('.list-films')
 
 // function trend() {
 //     return fetch(`${TREND_URL}${API_KEY}`) .then(response => {
@@ -14,3 +14,18 @@ treiler().then(data => {
     console.log(data)
   })
 // function render ()
+
+
+function render (films) {
+    const [{name, }] = films
+    list.innerHTML = ''
+    const rendering = films.map([{name}] => {
+        return ` <li class="film-item">
+        <img src="${}" alt="${}" width='394' height='574'>
+    <h1 class="title">${}</h1>
+    <p class="ganre">${}</p>
+      </li>`
+    })
+
+    list.insertAdjacentHTML('beforeend', rendering)
+}
