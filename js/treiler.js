77a6b434434344d3c25030ada1af862ee35381de
TarasTikan <tikantaras1@gmail.com>
2023-01-12 14:36:54 +0200

    Update treiler.js

--- js/treiler.js
+++ js/treiler.js
@@ -2,19 +2,19 @@
 // const TREILERS = `${BASE_URL}/movie/${id}/videos?api_key=`;
 // const API_KEY = '9f3449ca8495a13b6d35e887839f0061';
 // 899112
-const sss = document.querySelector('h1')
+// const sss = document.querySelector('h1')
 
-const ffff = document.querySelector('img')
-const list = document.querySelector('.list-films')
-treil.addEventListener('click', onClickWatch)
+// const ffff = document.querySelector('img')
+// const list = document.querySelector('.list-films')
+// treil.addEventListener('click', onClickWatch)
 
-    function onClickWatch (event) {
-        let query = event.target
-        console.log(query)
-    }
+//     function onClickWatch (event) {
+//         let query = event.target
+//         console.log(query)
+//     }
 
-    const ff = list.children
-    console.log(ffff)
+//     const ff = list.children
+//     console.log(ffff)
 //   console.log(treil)
 // console.log(treil)
 // function onClickWatch (event) {
