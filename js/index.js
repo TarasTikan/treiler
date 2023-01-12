0cce6d84fa6a75b9b58479570385bb2c83d6b6c4
TarasTikan <tikantaras1@gmail.com>
2023-01-12 21:42:51 +0200

    add dada

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
 import { treiler, trend } from './fetch.js'
+// import { renderTrail} from './treiler.js'
 // const API_KEY = '9f3449ca8495a13b6d35e887839f0061';
 // const BASE_URL = 'https://api.themoviedb.org/3';
 // const TREND_URL = `${BASE_URL}/trending/movie/week?api_key=`;
@@ -9,6 +10,58 @@ const API_URL = 'https://image.tmdb.org/t/p/w500'
 
   trend().then(data => {
    render(data.results)
+   const div = document.querySelector('.wrap')
+   console.log(div)
+   list.addEventListener('click', onClickWatch)
+
+   function onClickWatch (event) {
+    let query = event.target
+    if(event.target.nodeName !== 'IMG') {
+        return
+    }
+    treiler(query.id).then(data => {
+const officialTrail = data.results.length -1
+        console.log(data.results[officialTrail])
+        renderTrail(data.results[officialTrail])
+      })
+}
+function renderTrail (video) {
+  // div.innerHTML = ''
+  // const rendering = video.map(muvie => {
+    const {key} = video
+    div.innerHTML = `<iframe
+    width="394"
+    height="574"
+    src="https://www.youtube.com/embed/${key}"
+    title="YouTube video player"
+    frameborder="0"
+    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+    allowfullscreen
+    waitUntil()
+  ></iframe>`
+  // })
+    //   const {key} = video
+    //   div.innerHTML = `<iframe
+    //   width="394"
+    //   height="574"
+    //   src="https://www.youtube.com/embed/${key}"
+    //   title="YouTube video player"
+    //   frameborder="0"
+    //   allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+    //   allowfullscreen
+    //   waitUntil()
+    // ></iframe>`
+    // setTimeout(() => {
+    //   console.clear()
+    // }, 3000);
+    // setTimeout(() => {
+    //   console.clear()
+    // }, 3000);
+    // setTimeout(() => {
+    //   console.clear()
+    // }, 4000);
+    // div.insertAdjacentHTML('beforeend', rendering)
+}
   })
 
 function render (results) {
--- js/treiler.js
+++ js/treiler.js
@@ -2,45 +2,45 @@ import { treiler } from './fetch.js'
 
 const list = document.querySelector('.list-films')
 const div = document.querySelector('div')
-list.addEventListener('click', onClickWatch)
+// list.addEventListener('click', onClickWatch)
 
-    function onClickWatch (event) {
-        let query = event.target
-        if(event.target.nodeName !== 'IMG') {
-            return
-        }
-        console.log(query.id)
-        console.log(event.target)
-        treiler(query.id).then(data => {
-const officialTrail = data.results.length -1
-            console.log(data.results[officialTrail])
-            renderTrail(data.results[officialTrail])
-          //  render(data.results)
-          })
-    }
-
-    function renderTrail (video) {
-        // console.clear()
-            const {key} = video
-            div.innerHTML = `<iframe
-            width="560"
-            height="315"
-            src="https://www.youtube.com/embed/${key}"
-            title="YouTube video player"
-            frameborder="0"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-            allowfullscreen
-            waitUntil()
-          ></iframe>`
-          setTimeout(() => {
-            console.clear()
-          }, 3000);
-          setTimeout(() => {
-            console.clear()
-          }, 3000);
-          setTimeout(() => {
-            console.clear()
-          }, 4000);
-        // body.insertAdjacentHTML('beforeend', rendering)
-    }
+//     function onClickWatch (event) {
+//         let query = event.target
+//         if(event.target.nodeName !== 'IMG') {
+//             return
+//         }
+//         // console.log(query.id)
+//         console.log(event.target)
+//         treiler(query.id).then(data => {
+// const officialTrail = data.results.length -1
+//             console.log(data.results[officialTrail])
+//             renderTrail(data.results[officialTrail])
+//           //  render(data.results)
+//           })
+//     }
+// // 394  574
+//    export function renderTrail (video) {
+//         // console.clear()
+//             const {key} = video
+//             div.innerHTML = `<iframe
+//             width="560"
+//             height="315"
+//             src="https://www.youtube.com/embed/${key}"
+//             title="YouTube video player"
+//             frameborder="0"
+//             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+//             allowfullscreen
+//             waitUntil()
+//           ></iframe>`
+//           setTimeout(() => {
+//             console.clear()
+//           }, 3000);
+//           setTimeout(() => {
+//             console.clear()
+//           }, 3000);
+//           setTimeout(() => {
+//             console.clear()
+//           }, 4000);
+//         // body.insertAdjacentHTML('beforeend', rendering)
+//     }
 
