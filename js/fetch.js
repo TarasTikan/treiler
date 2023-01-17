8333531cbcc75384ccaa84546f80472c1a20ee36
TarasTikan <tikantaras1@gmail.com>
2023-01-18 00:58:29 +0200

    add dadada

--- js/fetch.js
+++ js/fetch.js
@@ -1,12 +1,10 @@
 const API_KEY = '9f3449ca8495a13b6d35e887839f0061';
 const BASE_URL = 'https://api.themoviedb.org/3';
-const TREND_URL = `${BASE_URL}/trending/movie/week?api_key=`;
-const TREILERS = `${BASE_URL}/movie/`;
-const watchId = `/videos?api_key=` 
+const ANONS_URL = `${BASE_URL}/movie/upcoming?api_key=`;
+const watchId = `&language=en-US&page=1` 
 
-
-export function treiler(id) {
-    return fetch(`${TREILERS}${id}${watchId}${API_KEY}`) .then(r => {
+export function anons(page) {
+    return fetch(`${ANONS_URL}${API_KEY}&language=en-US&page=${page}`).then(r => {
         if(!r.ok) {
             throw new Error(r.status)
         }
@@ -14,13 +12,13 @@ export function treiler(id) {
       })
   }
 
- export function trend() {
-    return fetch(`${TREND_URL}${API_KEY}`) .then(r => {
-        if(!r.ok) {
-            throw new Error(r.status)
-        }
-        return r.json();
-      })
-    }
-    https://api.themoviedb.org/3/movie/upcoming?api_key=<<api_key>>&language=en-US&page=1
+//  export function trend() {
+//     return fetch(`${TREND_URL}${API_KEY}`) .then(r => {
+//         if(!r.ok) {
+//             throw new Error(r.status)
+//         }
+//         return r.json();
+//       })
+//     }
+    // https://api.themoviedb.org/3/movie/upcoming?api_key=<<api_key>>&language=en-US&page=1
   
--- js/index.js
+++ js/index.js
@@ -1,6 +1,119 @@
-import { treiler, trend } from './fetch.js'
-// import { renderTrail} from './treiler.js'
-const list = document.querySelector('.list-films')
-const treil = document.querySelector('.treilers')
-const API_URL = 'https://image.tmdb.org/t/p/w500'
+import { anons } from './fetch.js'
+import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.esm.browser.min.js'
+const listFilm = document.querySelector('.swiper-wrapper')
+const itemFilm = document.querySelector('.swiper-slide')
+const nextBtn = document.querySelector('.swiper-button-next')
+const prevBtn = document.querySelector('.swiper-button-prev')
 
+
+nextBtn.addEventListener('click', nextPage)
+let page = 1
+function nextPage (e) {
+    anons(page).then(data => {
+        page += 1
+        listFilm.innerHTML = ''
+        renderAnonce(data.results)
+        console.log(data)
+        const swiper = new Swiper('.swiper', {
+            slidesPerView: 7,
+            // spaceBetween: 30,
+            // Optional parameters
+            // direction: 'vertical',
+            loop: true,
+            // spaceBetween: 100,
+            // If we need pagination
+            pagination: {
+              el: '.swiper-pagination',
+            },
+            // Navigation arrows
+            navigation: {
+              nextEl: '.swiper-button-next',
+              prevEl: '.swiper-button-prev',
+            },
+          });
+    })
+}
+
+
+
+
+
+// prevBtn.addEventListener('click', prevPage)
+
+// function prevPage (e) {
+//     anons(page).then(data => {
+//         page -= 1
+//         // listFilm.innerHTML = ''
+//         renderAnonce(data.results)
+//         console.log(data)
+//         const swiper = new Swiper('.swiper', {
+//             slidesPerView: 7,
+//             // spaceBetween: 30,
+//             // Optional parameters
+//             // direction: 'vertical',
+//             loop: true,
+//             // spaceBetween: 100,
+//             // If we need pagination
+//             pagination: {
+//               el: '.swiper-pagination',
+//             },
+//             // Navigation arrows
+//             navigation: {
+//               nextEl: '.swiper-button-next',
+//               prevEl: '.swiper-button-prev',
+//             },
+//           });
+//     })
+// }
+
+
+
+
+
+// anons().then(data => {
+//     renderAnonce(data.results)
+//     // nextBtn.addEventListener('click', nextPage)
+//     const swiper = new Swiper('.swiper', {
+//         slidesPerView: 7,
+//         // spaceBetween: 30,
+//         // Optional parameters
+//         // direction: 'vertical',
+//         loop: true,
+//         // spaceBetween: 100,
+//         // If we need pagination
+//         pagination: {
+//           el: '.swiper-pagination',
+//         },
+//         // Navigation arrows
+//         navigation: {
+//           nextEl: '.swiper-button-next',
+//           prevEl: '.swiper-button-prev',
+//         },
+//       });
+// })
+
+
+function renderAnonce (data){
+    const filmItem = data.map(({poster_path})=>{ 
+        return `<li class="swiper-slide"><img src="https://image.tmdb.org/t/p/w500${poster_path}" alt="Poster Film"></li>`
+    }).join('')
+
+    listFilm.insertAdjacentHTML('beforeend', filmItem)
+}
+
+
+// const swiper = new Swiper('.swiper', {
+//     // Optional parameters
+//     // direction: 'vertical',
+//     loop: true,
+//     // spaceBetween: 100,
+//     // If we need pagination
+//     pagination: {
+//       el: '.swiper-pagination',
+//     },
+//     // Navigation arrows
+//     navigation: {
+//       nextEl: '.swiper-button-next',
+//       prevEl: '.swiper-button-prev',
+//     },
+//   });
