869bb8bfb7270a307b52c19bcbda4cd204c075df
TarasTikan <tikantaras1@gmail.com>
2023-01-25 21:16:32 +0200

    add dsadss

--- js/index.js
+++ js/index.js
@@ -1,119 +1,65 @@
-import { anons } from './fetch.js'
-import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.esm.browser.min.js'
 const listFilm = document.querySelector('.swiper-wrapper')
-const itemFilm = document.querySelector('.swiper-slide')
-const nextBtn = document.querySelector('.swiper-button-next')
-const prevBtn = document.querySelector('.swiper-button-prev')
+listFilm.addEventListener('click', openModal)
 
-
-// nextBtn.addEventListener('click', nextPage)
-// let page = 1
-// function nextPage (e) {
-//     anons(page).then(data => {
-//         page += 1
-//         listFilm.innerHTML = ''
-//         renderAnonce(data.results)
-//         console.log(data)
-//         const swiper = new Swiper('.swiper', {
-//             slidesPerView: 7,
-//             // spaceBetween: 30,
-//             // Optional parameters
-//             // direction: 'vertical',
-//             loop: true,
-//             // spaceBetween: 100,
-//             // If we need pagination
-//             pagination: {
-//               el: '.swiper-pagination',
-//             },
-//             // Navigation arrows
-//             navigation: {
-//               nextEl: '.swiper-button-next',
-//               prevEl: '.swiper-button-prev',
-//             },
-//           });
-//     })
-// }
-
-
-
-
-
-// prevBtn.addEventListener('click', prevPage)
-
-// function prevPage (e) {
-//     anons(page).then(data => {
-//         page -= 1
-//         // listFilm.innerHTML = ''
-//         renderAnonce(data.results)
-//         console.log(data)
-//         const swiper = new Swiper('.swiper', {
-//             slidesPerView: 7,
-//             // spaceBetween: 30,
-//             // Optional parameters
-//             // direction: 'vertical',
-//             loop: true,
-//             // spaceBetween: 100,
-//             // If we need pagination
-//             pagination: {
-//               el: '.swiper-pagination',
-//             },
-//             // Navigation arrows
-//             navigation: {
-//               nextEl: '.swiper-button-next',
-//               prevEl: '.swiper-button-prev',
-//             },
-//           });
-//     })
-// }
-
-
-
-
-
-anons().then(data => {
-    renderAnonce(data.results)
-    // nextBtn.addEventListener('click', nextPage)
-    const swiper = new Swiper('.swiper', {
-        slidesPerView: 7,
-        // spaceBetween: 30,
-        // Optional parameters
-        // direction: 'vertical',
-        loop: true,
-        // spaceBetween: 100,
-        // If we need pagination
-        pagination: {
-          el: '.swiper-pagination',
-        },
-        // Navigation arrows
-        navigation: {
-          nextEl: '.swiper-button-next',
-          prevEl: '.swiper-button-prev',
-        },
-      });
+fetchUncomingFilms(1).then(res => {
+  renderAnonce(res.data.results)
+  const swiper = new Swiper('.card_slider', {
+      modules: [Navigation],
+      slidesPerView: 8,
+      speed: 700,
+      spaceBetween: 30,
+      breakpoints: {
+          // when window width is >= 320px
+          320: {
+            slidesPerView: 2,
+            spaceBetween: 20
+          },
+          // when window width is >= 480px
+          480: {
+            slidesPerView: 3,
+            spaceBetween: 30
+          },
+          // when window width is >= 640px
+          640: {
+            slidesPerView: 4,
+            spaceBetween: 40
+          },
+          1280: {
+            slidesPerView: 7,
+            spaceBetween: 40
+          },
+          1300: {
+            slidesPerView: 8,
+            spaceBetween: 40
+          }},   
+      // Optional parameters
+      // direction: 'vertical',
+      hoverpause: true,
+bound: true,
+      loop: true,
+autoplay: {
+  delay: 1000,
+  disableOnInteraction: false,       
+},
+      // spaceBetween: 100,
+      // If we need pagination
+      pagination: {
+        el: '.swiper-pagination',
+      },
+      // Navigation arrows
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    });
 })
 
 
+
 function renderAnonce (data){
-    const filmItem = data.map(({poster_path})=>{ 
-        return `<li class="swiper-slide"><img src="https://image.tmdb.org/t/p/w500${poster_path}" alt="Poster Film"></li>`
-    }).join('')
+  const filmItem = data.map(({poster_path, id})=>{ 
+      return `<li class="swiper-slide"><img src="https://image.tmdb.org/t/p/w500${poster_path}" alt="Poster Film" class="img-slider photo__card" id="${id}"></li>`
+  }).join('')
 
-    listFilm.insertAdjacentHTML('beforeend', filmItem)
+  listFilm.insertAdjacentHTML('beforeend', filmItem)
 }
-
-
-// const swiper = new Swiper('.swiper', {
-//     // Optional parameters
-//     // direction: 'vertical',
-//     loop: true,
-//     // spaceBetween: 100,
-//     // If we need pagination
-//     pagination: {
-//       el: '.swiper-pagination',
-//     },
-//     // Navigation arrows
-//     navigation: {
-//       nextEl: '.swiper-button-next',
-//       prevEl: '.swiper-button-prev',
-//     },
-//   });
